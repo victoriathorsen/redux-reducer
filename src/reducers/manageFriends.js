1c67b30378135cf26c3265c4ca5ebf03322b8443
Victoria <victoriathorsenn@gmail.com>
2021-07-12 22:21:19 -0700

    commit tree 9116a7053404afef082680d4cad7809f357a4267

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,29 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: []
+}, action){
+    switch(action.type){
+        case 'ADD-FRIEND':
+            return (
+                {
+                    ...state,
+                    friends: [
+                        ...state.friends,
+                        action.friend
+                    ]
+                }
+            )
+        case 'REMOVE-FRIEND':
+            const removalIndx = state.friends.findIndex(friend => friend.id === action.id)
+            return (
+                {
+                    ...state,
+                        friends: [
+                            ...state.friends.slice(0, removalIndx),
+                            ...state.friends.slice(removalIndx + 1)
+                        ]
+                }
+            )
+        default:
+            return state;
+    }
 }
